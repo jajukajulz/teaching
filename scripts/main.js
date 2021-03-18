9e6c9cd0764c715c6c8fdef4fa64c3403d928e34
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-03-18 10:56:32 +0200

    add HTML form and JS to process the submission

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,14 @@ function myFunction() {
   }
   document.getElementById("demo").innerHTML = txt;
 }
+
+function processFormSubmission() {
+  var first_name = document.getElementById("fname").value;
+  var last_name = document.getElementById("lname").value;
+
+  document.getElementById("form_fname").innerHTML = first_name;
+  document.getElementById("form_lname").innerHTML = last_name;
+
+  // do not submit the form
+  return false;
+}
