1235247ded454d6c05d380bae95edb93a9db2160
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-03-29 01:09:12 +0200

    add ejs config and route for handling form submission

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const sqlite3 = require("sqlite3").verbose(); // “.verbose()” method allows 
 // Creating the Express server
 const app = express();
 
+//Set view engine to ejs
+app.set("view engine", "ejs");
+
 //Creating the Express router
 const router = express.Router();
 
-//serve static files in express
-app.use(express.static(path.join(__dirname, "public"))); //then e.g. this will work http://localhost:3000/images/firefox-icon.png
+// Configure middleware
+app.use(express.static(path.join(__dirname, "public"))); //serve static files in express then e.g. this will work http://localhost:3000/images/firefox-icon.png
+app.use(express.urlencoded({ extended: false })); // use the middleware “express.urlencoded()” so that request.body retrieves the posted values
 
 // Connection to the SQlite database
 const db_name = path.join(__dirname, "data", "apptest.db");
@@ -33,9 +37,9 @@ db.run(sql_create, (err) => {
   console.log("Successful creation of the 'User' table");
 
   // Database seeding
-  const sql_insert = `INSERT INTO User (User_ID, Name, Surname) VALUES
-  (1, 'John', 'Doe'),
-  (2, 'Bill', 'Gates');`;
+  const sql_insert = `INSERT INTO User (Name, Surname) VALUES
+  ('John', 'Doe'),
+  ('Bill', 'Gates');`;
   db.run(sql_insert, (err) => {
     if (err) {
       return console.error(err.message);
@@ -44,17 +48,39 @@ db.run(sql_create, (err) => {
   });
 });
 
+// GET /
 router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
-  //__dirname : It will resolve to your project folder.
+  res.render(__dirname + "/views/index.ejs", { status: "No status" }); //__dirname resolves to your project folder.
+
+  //The sendfile method, on the other hand, simply sends a given file to the client, regardless of the type and contents of the file.
+  //render allows processing of variables but requires use of a templating engine e.g. name
+  //  res.sendFile(
+  //    path.join(__dirname + "/views/index.html")
+  //  );
 });
 
+// GET /about
 router.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
+// POST /
+router.post("/", function (req, res) {
+  const user = [req.body.fname, req.body.lname];
+  console.log("Submitted name: " + req.body.fname);
+  console.log("Submitted surname: " + req.body.lname);
+  const sql = "INSERT INTO User (Name, Surname) VALUES (?,?)";
+  db.run(sql, user, (err) => {
+    // if (err) ...
+    res.render(__dirname + "/views/index.ejs", { status: "Saved to DB" });
+    //res.redirect("/");
+  });
+});
+
 //add the router
 app.use("/", router);
-app.listen(process.env.port || 3000);
 
-console.log("Running at Port 3000");
+// Starting the server
+app.listen(3000, () => {
+  console.log("Server started (http://localhost:3000/) !");
+});
