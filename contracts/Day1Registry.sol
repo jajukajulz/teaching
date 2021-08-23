44991961971ca386662965a42937436ae70fa517
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-08-23 07:05:56 +0200

    add event for successful add to blockchain

--- contracts/Day1Registry.sol
+++ contracts/Day1Registry.sol
@@ -9,12 +9,26 @@ contract Day1Registry {
     address added_by;
   }
 
+  // event fired on submission of a Day1User entry.
+  event registeredDay1UserEvent (
+    string _name,
+    string _surname,
+    uint _submissionBlockNumber
+  );
+
+
   // define the array of users
   Day1User[] public day1_users;
 
   function registerUser(string calldata _name, string calldata _surname) external returns(uint){
+
+    uint256 submissionBlockNumber = block.number;
+
     // get an instance of a Day1User using the input variables and push into the array of day1_users, returns the id
     uint id = day1_users.push(Day1User(_name, _surname, msg.sender)) - 1;
+
+    // trigger event for Day1User registration
+    emit registeredDay1UserEvent(_name, _surname, submissionBlockNumber);
     
     // return the user id
     return id;
